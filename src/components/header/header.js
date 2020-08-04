055f53b13f557d8c25864780632a86d1bc103224
ibraim-Ajarmeh <ajarmeh1997@gmail.com>
2020-08-04 10:49:50 +0300

    fixed the music button

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -8,18 +8,49 @@ function Header() {
 
     useEffect(() => {
         let audio = document.querySelector('audio');
-        $('.fa-music').click(function (e) {
+        $('.fa-volume-up').click(function (e) {
             e.preventDefault();
-            if (audio.muted === false) {
-                $('audio').prop('muted', true);
-                $('.fa-music::after').css('disply', 'unset');
-                
-            } else {
-                $('audio').prop('muted', false);
-                $('.fa-music::after').css('disply', 'none');
-            }
+            $('audio').prop('muted', true);
+            // $('iframe').prop('muted', true);
+            $('.fa-volume-up').css('disply', 'none');
+            $('.fa-volume-mute').css('disply', 'unset');
         });
+
+        $('.fa-volume-mute').click(function (e) {
+            e.preventDefault();
+            $('audio').prop('muted', false);
+            // $('iframe').prop('muted', false);
+            $('.fa-volume-up').css('disply', 'unset');
+            $('.fa-volume-mute').css('disply', 'none');
+        });
+
+
+        let isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+        if (!isChrome) {
+            console.log('It is not Chrome')
+            // $('#playAudio').remove();
+        } else {
+            // $('#iframeAudio').remove();
+            console.log('It is Chrome')
+        }
+        console.log('hello', isChrome)
     }, []);
+
+    const muteFunc = () => {
+        let audio = document.getElementById('playAudio');
+        audio.setAttribute('mute', true);
+
+        $('#mute-icon').fadeOut(500);
+        $('#unmute-icon').fadeIn(500);
+    }
+    
+    const unMuteFunc = () => {
+        let audio = document.getElementById('playAudio')
+        audio.setAttribute('mute', false)
+        
+        $('#mute-icon').fadeIn(500);
+        $('#unmute-icon').fadeOut(500);
+    }
 
     return (
         <header>
@@ -27,8 +58,10 @@ function Header() {
                 <h1>Naseem Izzat</h1>
                 <h3>curriculum vitae</h3>
             </div>
-            <i className='fa fa-music'></i>
-            <audio src={always} loop autoPlay ></audio>
+            <i className='fas fa-volume-up' id='mute-icon' onClick={muteFunc}></i>
+            <i className='fas fa-volume-mute' id='unmute-icon' onClick={unMuteFunc}></i>
+            <audio src={always} loop autoPlay id='playAudio' ></audio>
+            {/* <iframe src={always} allow='autoplay' style={{ display: 'none' }} id='iframeAudio'></iframe> */}
         </header>
     )
 }
